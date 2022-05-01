59f8144dc4f6d5c812f50af851fd273c495a916e
agirzhev <a.girgev@gmail.com>
2022-05-01 15:52:22 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const element = galleryItems.map((image) => '<div class="gallery__item"><a class="gallery__link" href="'+image.original+'" > <img class="gallery__image" src="'+image.preview+'" data-source="'+image.original+'" alt="'+image.description+'"/></a></div>').join('');
+gallery.insertAdjacentHTML('beforeEnd', element);
+  
+  
+document.querySelector(".gallery__link").addEventListener("click", function (event) {
+  event.preventDefault()
+  //basicLightbox.create(`
+	//	<img width="1400" height="900" src="${event.original}">
+	//`).show()
+  console.log(event)
+})
