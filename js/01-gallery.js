aa409d360e3a2af568c2df6c481ebbb3b8fd0b25
agirzhev <a.girgev@gmail.com>
2022-05-14 17:00:38 +0200

    allfinish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+//import * as basicLightbox from 'basiclightbox'
+
 const gallery = document.querySelector(".gallery");
 
-const element = galleryItems.map((image) => '<div class="gallery__item"><a class="gallery__link" href="'+image.original+'" > <img class="gallery__image" src="'+image.preview+'" data-source="'+image.original+'" alt="'+image.description+'"/></a></div>').join('');
+const element = galleryItems.map((image) => '<div class="gallery__item modal"><a class="gallery__link" href="'+image.original+'" > <img class="gallery__image" src="'+image.preview+'" data-source="'+image.original+'" alt="'+image.description+'"/></a></div>').join('');
 gallery.insertAdjacentHTML('beforeEnd', element);
   
   
 gallery.addEventListener("click", onclick);
   
 function onclick(event) {
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
-    }
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
     
   const instance = basicLightbox.create(`
-        <img src="${event.target.dataset.source}"/> 
+    <img src="${event.target.dataset.source}"/> 
 `, {
     onShow: instance => { window.addEventListener('keydown', onEscPress); },
     onClose: instance => { window.removeEventListener('keydown', onEscPress); }
@@ -29,5 +32,3 @@ function onclick(event) {
   }
 }
 }
-
-
